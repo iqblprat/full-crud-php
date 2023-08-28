7a6d6654f3b4ce0cde6b50bac6f76ec056a01937
iqblprat <iiqbalpratt@gmail.com>
2023-08-28 09:08:17 +0700

    perubahan pertama

---
+++ code-lama/mahasiswa2.php
@@ -0,0 +1,68 @@
+<?php
+
+session_start();
+
+if (!isset($_SESSION['login'])) {
+    echo "<script>
+            document.location.href = 'login.php';
+          </script>";
+    exit;
+}
+
+if ($_SESSION['level'] != 1 and $_SESSION['level'] != 3) {
+    echo "<script>
+            alert('Anda tidak memiliki hak akses.');
+            document.location.href = 'index.php';
+          </script>";
+    exit;
+}
+
+$title = "Data Mahasiswa";
+
+include 'layout/header.php';
+
+//menampilkan data mahasiswa
+$data_mahasiswa = select("SELECT * FROM mahasiswa ORDER BY id_mahasiswa DESC");
+?>
+
+<div class="container mt-5 mb-3">
+    <h1><i class="fas fa-user-graduate"></i> Data Mahasiswa</h1>
+    <hr>
+    <a href="tambah-mahasiswa.php" class="btn btn-primary mb-1"><i class="fas fa-plus-circle"></i> Tambah</a>
+    <a href="download-excel-mahasiswa.php" class="btn btn-success mb-1"><i class="fas fa-file-excel"></i> Download Excel</a>
+    <a href="download-pdf-mahasiswa.php" class="btn btn-danger mb-1"><i class="fas fa-file-pdf"></i> Download PDF</a>
+    <table class="table table-bordered table-striped" id="table">
+        <thead>
+            <tr>
+                <th>No</th>
+                <th>Nama</th>
+                <th>Prodi</th>
+                <th>Jenis Kelamin</th>
+                <th>Nomor Telepon</th>
+                <th>Email</th>
+                <th>Aksi</th>
+            </tr>
+        </thead>
+        <tbody>
+            <?php $no = 1; ?>
+            <?php foreach ($data_mahasiswa as $mahasiswa) : ?>
+                <tr>
+                    <td><?= $no++; ?></td>
+                    <td><?= $mahasiswa['nama']; ?></td>
+                    <td><?= $mahasiswa['prodi']; ?></td>
+                    <td><?= $mahasiswa['jk']; ?></td>
+                    <td><?= $mahasiswa['telepon']; ?></td>
+                    <td><?= $mahasiswa['email']; ?></td>
+                    <td class="text-center" width="21%">
+                        <a href="detail-mahasiswa.php?id_mahasiswa=<?= $mahasiswa['id_mahasiswa']; ?>" class="btn btn-secondary btn-sm"><i class="fas fa-info"></i> Detail</a>
+                        <a href="ubah-mahasiswa.php?id_mahasiswa=<?= $mahasiswa['id_mahasiswa']; ?>" class="btn btn-success btn-sm"><i class="fas fa-edit"></i> Ubah</a>
+                        <a href="hapus-mahasiswa.php?id_mahasiswa=<?= $mahasiswa['id_mahasiswa']; ?>" onclick="return confirm('Yakin Data Barang akan Dihapus?')" class="btn btn-danger btn-sm"><i class="fas fa-trash"></i> Hapus</a>
+
+                    </td>
+                </tr>
+            <?php endforeach; ?>
+        </tbody>
+    </table>
+</div>
+
+<?php include 'layout/footer.php'; ?>
--- ubah-mahasiswa.php
+++ ubah-mahasiswa.php
@@ -33,65 +33,67 @@ $mahasiswa = select("SELECT * FROM mahasiswa WHERE id_mahasiswa = $id_mahasiswa"
 
 ?>
 
-<div class="container mt-5">
-    <h1>Ubah Data Mahasiswa</h1>
-    <hr>
-    <form action="" method="post" enctype="multipart/form-data">
-        <input type="hidden" name="id_mahasiswa" value="<?= $mahasiswa['id_mahasiswa']; ?>">
-        <input type="hidden" name="fotoLama" value="<? $mahasiswa['foto']; ?>">
-        <div class="mb-3">
-            <label for="nama" class="form-label">Nama</label>
-            <input type="text" class="form-control" id="nama" name="nama" placeholder="Nama Mahasiswa..." value="<?= $mahasiswa['nama']; ?>" required>
-        </div>
-
-        <div class="row">
-            <div class="mb-3 col-6">
-                <label for="prodi" class="form-label">Program Studi</label>
-                <select name="prodi" id="prodi" class="form-control">
-                    <?php $prodi = $mahasiswa['prodi']; ?>
-                    <option value="">-- pilih program studi --</option>
-                    <option value="Teknik Informatika" <?= $prodi == 'Teknik Informatika' ? 'selected' : null ?>>Teknik Informatika</option>
-                    <option value="Teknik Mesin" <?= $prodi == 'Teknik Mesin' ? 'selected' : null ?>>Teknik Mesin</option>
-                    <option value="Teknik Listrik" <?= $prodi == 'Teknik Listrik' ? 'selected' : null ?>>Teknik Listrik</option>
-                </select>
+<!-- Content Wrapper. Contains page content -->
+<div class="content-wrapper">
+    <div class="container mt-5">
+        <h1>Ubah Data Mahasiswa</h1>
+        <hr>
+        <form action="" method="post" enctype="multipart/form-data">
+            <input type="hidden" name="id_mahasiswa" value="<?= $mahasiswa['id_mahasiswa']; ?>">
+            <input type="hidden" name="fotoLama" value="<? $mahasiswa['foto']; ?>">
+            <div class="mb-3">
+                <label for="nama" class="form-label">Nama</label>
+                <input type="text" class="form-control" id="nama" name="nama" placeholder="Nama Mahasiswa..." value="<?= $mahasiswa['nama']; ?>" required>
             </div>
-            <div class="mb-3 col-6">
-                <label for="jk" class="form-label">Jenis Kelamin</label>
-                <select name="jk" id="jk" class="form-control">
-                    <?php $jk = $mahasiswa['jk']; ?>
-                    <option value="">-- pilih jenis kelamin --</option>
-                    <option value="Laki-Laki" <?= $jk == 'Laki-Laki' ? 'selected' : null ?>>Laki-Laki</option>
-                    <option value="Perempuan" <?= $jk == 'Perempuan' ? 'selected' : null ?>>Perempuan</option>
-                </select>
+
+            <div class="row">
+                <div class="mb-3 col-6">
+                    <label for="prodi" class="form-label">Program Studi</label>
+                    <select name="prodi" id="prodi" class="form-control">
+                        <?php $prodi = $mahasiswa['prodi']; ?>
+                        <option value="">-- pilih program studi --</option>
+                        <option value="Teknik Informatika" <?= $prodi == 'Teknik Informatika' ? 'selected' : null ?>>Teknik Informatika</option>
+                        <option value="Teknik Mesin" <?= $prodi == 'Teknik Mesin' ? 'selected' : null ?>>Teknik Mesin</option>
+                        <option value="Teknik Listrik" <?= $prodi == 'Teknik Listrik' ? 'selected' : null ?>>Teknik Listrik</option>
+                    </select>
+                </div>
+                <div class="mb-3 col-6">
+                    <label for="jk" class="form-label">Jenis Kelamin</label>
+                    <select name="jk" id="jk" class="form-control">
+                        <?php $jk = $mahasiswa['jk']; ?>
+                        <option value="">-- pilih jenis kelamin --</option>
+                        <option value="Laki-Laki" <?= $jk == 'Laki-Laki' ? 'selected' : null ?>>Laki-Laki</option>
+                        <option value="Perempuan" <?= $jk == 'Perempuan' ? 'selected' : null ?>>Perempuan</option>
+                    </select>
+                </div>
             </div>
-        </div>
 
-        <div class="mb-3">
-            <label for="telepon" class="form-label">Nomor Telepon</label>
-            <input type="number" class="form-control" id="telepon" name="telepon" placeholder="Nomor Telepon Mahasiswa..." value="<?= $mahasiswa['telepon']; ?>" required>
-        </div>
+            <div class="mb-3">
+                <label for="telepon" class="form-label">Nomor Telepon</label>
+                <input type="number" class="form-control" id="telepon" name="telepon" placeholder="Nomor Telepon Mahasiswa..." value="<?= $mahasiswa['telepon']; ?>" required>
+            </div>
 
-        <div class="mb-3">
-            <label for="alamat" class="form-label">Alamat</label>
-            <textarea id="alamat" name="alamat" placeholder="Alamat Mahasiswa..."><?= $mahasiswa['alamat']; ?></textarea>
-        </div>
+            <div class="mb-3">
+                <label for="alamat" class="form-label">Alamat</label>
+                <textarea id="alamat" name="alamat" placeholder="Alamat Mahasiswa..."><?= $mahasiswa['alamat']; ?></textarea>
+            </div>
 
-        <div class="mb-3">
-            <label for="email" class="form-label">Email</label>
-            <input type="email" class="form-control" id="email" name="email" placeholder="Alamat Email Mahasiswa..." value="<?= $mahasiswa['email']; ?>" required>
-        </div>
+            <div class="mb-3">
+                <label for="email" class="form-label">Email</label>
+                <input type="email" class="form-control" id="email" name="email" placeholder="Alamat Email Mahasiswa..." value="<?= $mahasiswa['email']; ?>" required>
+            </div>
 
-        <div class="mb-3">
-            <label for="foto" class="form-label">Foto</label>
-            <input type="file" class="form-control" id="foto" name="foto" placeholder="Foto Mahasiswa..." onchange="previewImg()">
+            <div class="mb-3">
+                <label for="foto" class="form-label">Foto</label>
+                <input type="file" class="form-control" id="foto" name="foto" placeholder="Foto Mahasiswa..." onchange="previewImg()">
 
-            <img src="assets/img/<?= $mahasiswa['foto']; ?>" alt="" class="img-thumbnail img-preview mt-3" width="100px">
-        </div>
+                <img src="assets/img/<?= $mahasiswa['foto']; ?>" alt="" class="img-thumbnail img-preview mt-3" width="100px">
+            </div>
 
-        <button type="submit" name="ubah" class="btn btn-primary" style="float: right;">Simpan</button>
-    </form>
+            <button type="submit" name="ubah" class="btn btn-primary" style="float: right;">Simpan</button>
+        </form>
+    </div>
 </div>
-
 <script>
     function previewImg() {
         const foto = document.querySelector('#foto');
@@ -105,5 +107,4 @@ $mahasiswa = select("SELECT * FROM mahasiswa WHERE id_mahasiswa = $id_mahasiswa"
         }
     }
 </script>
-
 <?php include 'layout/footer.php'; ?>
