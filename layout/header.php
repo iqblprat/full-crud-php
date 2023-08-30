33e0eb9d7c894355f5e3d1cccb7ef273c293bd6a
iqblprat <125345677+iqblprat@users.noreply.github.com>
2023-08-30 07:49:32 +0700

    sidebar

--- layout/header.php
+++ layout/header.php
@@ -70,7 +70,7 @@ include 'config/app.php';
                 <!-- Sidebar user panel (optional) -->
                 <div class="user-panel mt-3 pb-3 mb-3 d-flex">
                     <div class="image">
-                        <img src="assets-template/dist/img/user2-160x160.jpg" class="img-circle elevation-2" alt="User Image">
+                        <img src="assets-template/dist/img/admin.png" class="img-circle elevation-2" alt="User Image">
                     </div>
                     <div class="info">
                         <a href="#" class="d-block"><?= $_SESSION['nama']; ?></a>
